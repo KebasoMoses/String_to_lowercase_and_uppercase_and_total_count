05227f66c845859f2fb40cb454c061fb055b9588
Kebaso Moses <98042989+KebasoMoses@users.noreply.github.com>
2022-02-10 15:37:17 +0300

    Add files via upload

---
+++ helpers.js
@@ -0,0 +1,61 @@
+/** @param {string} name */
+export function getNumberOfChars(name) {
+    // return the number of characters in: name
+    return name.length
+}
+
+
+/** @param {string} name */
+export function getLower(name) {
+    // return name all in lower case (example: "ABC" becomes "abc")
+    return name.toLowerCase()
+
+}
+
+/** @param {string} name */
+export function getUpper(name) {
+    // return name all in upper case (example: "abc" becomes "ABC")
+    return name.toUpperCase()
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// export function getNumbersOfChars(name){
+//     return name.length
+// };
+
+// export function getLower(name){
+//     return name.toLowerCase()
+// };
+
+// export function getUpper(name){
+//     return name.toUpperCase
+// };
